15e1182269a90bdf4c48eeb82cbaa99b2915bdbb
Manuel Ochoa <manuelochoa2097@hotmail.com>
2019-12-02 12:14:56 -0600

    scala-intro

--- activities/classwork/scala-intro/src/main/scala/example/Hello.scala
+++ activities/classwork/scala-intro/src/main/scala/example/Hello.scala
@@ -3,6 +3,15 @@ package example
 object Hello {
   val default = "World"
 
-  def greeting(name: String = default): String = "Hello"
+  // Greeting function
+  // run with sbt "runMain example.Hello World"
+  def greeting(name: String = default): String = {  // expresion
+    s"Hello, $name!"
+  }
 
+  //Main method
+  def main(args: Array[String]): Unit = {
+    val name = args.headOption.getOrElse(default)
+    println(greeting(name))
+  }
 }
